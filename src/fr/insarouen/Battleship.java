ad26d80f97729954f106fccb9cc7954e90386b15
davHub <david.albert@insa-rouen.fr>
2018-04-18 22:03:53 +0200

    Mise en place de la structure du projet

---
+++ src/fr/insarouen/Battleship.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+package fr.insarouen;
+
+import fr.insarouen.*;
+import fr.insarouen.view.IHM;
+
+/**
+ * @author david
+ *
+ */
+public class Battleship {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		IHM ihm = new IHM();
+
+	}
+
+}
---
+++ src/fr/insarouen/view/IHM.java
@@ -0,0 +1,50 @@
+/**
+ * 
+ */
+package fr.insarouen.view;
+
+import javax.swing.*;
+import java.awt.*;
+import java.lang.*;
+
+/**
+ * @author david
+ *
+ */
+public class IHM extends JFrame {
+
+    public static final Color BACKGROUND_COLOR = new Color(0,0,0);
+    JPanel container = new JPanel();
+   
+    
+    
+    public IHM(){
+
+	// Initialiser fenêtre
+	super("BattleShip");
+	this.setSize(1000,600);
+	this.setBackground(BACKGROUND_COLOR);
+	this.setLocationRelativeTo(null); //au centre
+	this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //arrête le programme quand on appuie sur la croix
+	this.setResizable(true); //possibilité de redimensionner la fenêtre
+
+	// Paramétrage du conteneur principal
+	container.setLayout(new GridLayout());
+
+	// Menu
+	//this.setJMenuBar(menu);
+	
+	// Ajout du plan de l'infrastructure au conteneur principal
+	container.add(new Button("Cliquer pour commencer"));
+	
+	// Ajout West
+	
+
+
+	this.setContentPane(container);
+	
+	//Afficher la fenêtre
+	this.setVisible(true);
+	
+    }
+}
