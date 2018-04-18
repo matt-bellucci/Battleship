33d9a870891ff0e4008b03aebec6bb78cb243517
davHub <david.albert@insa-rouen.fr>
2018-04-18 23:50:38 +0200

    Ajout Barre Menu

---
+++ src/fr/insarouen/view/Menu.java
@@ -0,0 +1,36 @@
+package fr.insarouen.view;
+
+import javax.swing.*;
+import java.awt.*;
+
+
+public class Menu extends JMenuBar {
+
+	
+    private JPanel container = new JPanel();
+	
+    private JMenu newGame = new JMenu("Nouvelle partie");
+    private JMenu stats = new JMenu("Statistique");
+    private JMenu aide = new JMenu("?");
+	
+    private JMenuItem noNet = new JMenuItem("Mode hors ligne");
+    private JMenuItem net = new JMenuItem("Mode en ligne");
+    private JMenuItem item4 = new JMenuItem("Enregistrer sous");
+	
+	
+	public Menu(){
+	
+		
+	    // Menu
+	    this.newGame.add(noNet);
+	    this.newGame.add(net);
+	    
+	    
+	    this.add(newGame);
+	    this.add(stats);
+	    this.add(aide);
+	        
+	  
+	}
+
+}
