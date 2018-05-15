7dff957042707d422c3418164c0852eeda609cbd
Matthieu Bellucci <matt7469@gmail.com>
2018-05-15 11:19:15 +0200

    Ajout Point2D PlayerInGame

--- src/fr/insarouen/battleship/model/PlayerInGame.java
+++ src/fr/insarouen/battleship/model/PlayerInGame.java
@@ -1,5 +1,29 @@
 package fr.insarouen.battleship.model;
 
 public class PlayerInGame extends Player {
-
+    
+    private Grid playerGrid;
+    
+    
+    public PlayerInGame(){
+        super();
+        this.playerGrid = new Grid();          
+        
+    }
+    
+    public PlayerInGame(Player player){
+        super(player.getName(), player.getIP());
+        this.playerGrid = new Grid();
+    }
+    
+    public PlayerInGame(Player player, Grid grid){
+        super(player.getName(), player.getIP());
+        this.playerGrid = grid;
+    }
+    
+    
+    /*public Case getCase(Point2D point){
+        playerGrid.getCase(point).getId();
+    }*/
+    
 }
