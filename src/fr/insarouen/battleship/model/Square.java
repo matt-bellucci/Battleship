83b75338b32f27d004794961a80a8f5ee5d025a7
Tiranyk <thomas.anquetil76@gmail.com>
2018-05-18 19:00:34 +0200

    Ajout de Square.java et modif de Grid et Boat

---
+++ src/fr/insarouen/battleship/model/Square.java
@@ -0,0 +1,13 @@
+package fr.insarouen.battleship.model;
+
+public class Square {
+
+	private boolean discovered=false;
+	private int id;
+	
+	public void Square(){
+	
+		this.squareId = Boat.getId(); 
+	}	
+	
+}
