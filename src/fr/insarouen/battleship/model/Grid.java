afe84867b74cb0f19aface8a1aa3d9a6cc7b7ca4
davHub <david.albert@insa-rouen.fr>
2018-05-31 09:40:15 +0200

    Dernière modif

--- src/fr/insarouen/battleship/model/Grid.java
+++ src/fr/insarouen/battleship/model/Grid.java
@@ -1,8 +1,7 @@
 package fr.insarouen.battleship.model;
 
 import java.util.Random;
-import java.util.HashMap;
-import java.util.Collection;
+
 /**
 * Describes the game grid.
 */
@@ -30,8 +29,8 @@ public class Grid {
 		for (int i=0;i<this.size;i++){
 			for (int j=0;j<this.size;j++){
 				this.grid[i][j] = new Square();
+				}
 			}
-		}
 	}
 		
 	
@@ -41,16 +40,16 @@ public class Grid {
 
 	public boolean isDiscovered(int x, int y) {
 		return grid[x][y].isDiscovered();	
-	}
+		}
 	
 	/**
 	  * Allows to turn a square as a discovered one.
 	  */
 
 	public void discover(int x, int y) {
-		System.out.println("Decouvrir : " + x + y);
+		System.out.println("Decouvrir : "+x+y);
 		this.grid[x][y].discover();
-	}
+		}
 		
 	/**
 	  * Returns the id of a specific square of the grid.
@@ -58,7 +57,7 @@ public class Grid {
 
 	public int getId(int x, int y) {
 		return  grid[x][y].getId();
-	}
+		}
 	
 	/**
 	  * Returns the grid' size.
@@ -74,51 +73,40 @@ public class Grid {
 
 	public void placeBoatsRandomly(Boats playerBoats) {
 		Random randomGenerator = new Random();
-		for (Boat boat : playerBoats.values()){
+		for (int i=0; i< playerBoats.getNbBoats();i++){
 			boolean place = false;
 			while (!place){
 				int x = randomGenerator.nextInt(getSize());
 				int y = randomGenerator.nextInt(getSize());
 				int sens = randomGenerator.nextInt(2);
-				place = placeBoat(boat, sens, x, y);
+				int compteur = 0;
+				if ((x+playerBoats.getBoat(i).getLength()<getSize()) && (sens==0) ){
+					for (int j = 0;j<playerBoats.getBoat(i).getLength();j++){
+						if (grid[x+j][y].getId() < 0){
+							compteur++;
+						}	
+						if (compteur == playerBoats.getBoat(i).getLength()){
+							place = true;
+							for (int k = 0;k<playerBoats.getBoat(i).getLength();k++)					
+								this.grid[x+k][y].setId(i);
+						}
+					}
+				}
+				else if ((y+playerBoats.getBoat(i).getLength()<getSize()) && (sens==1)){
+					for (int j = 0;j<playerBoats.getBoat(i).getLength();j++){
+						if (grid[x][y+j].getId() < 0){
+							compteur++;
+						}
+						if (compteur == playerBoats.getBoat(i).getLength()){
+							place = true;
+							for (int k = 0;k<playerBoats.getBoat(i).getLength();k++)					
+								this.grid[x][y+k].setId(i);
+						}
+					}
+				}
+			
 			}
 		}
-	}
-	
-	private void placeBoatOnCase(Boat boat, int sens, int x, int y){
-		for (int k = 0; k < boat.getLength(); k++){
-			if (sens == 0) { this.grid[x+k][y].setId(boat.getId());}
-			else { this.grid[x][y+k].setId(boat.getId());}
-		}
-	}
-	
-	private boolean testPlace(Boat boat, int sens, int x, int y){
-		if ( ( x + boat.getLength() < getSize() ) && (sens==0) ){
-			int j = 0;
-			while ( (j <= boat.getLength()) && (this.grid[x+j][y].getId() < 0)){
-				j++;
-			}
-			return ( (j+1) == boat.getLength()); 
-		}
-		
-		else if ( ( y + boat.getLength() < getSize() ) && (sens==1) ){
-			int j = 0;
-			while ( (j <= boat.getLength()) && (this.grid[x][y+j].getId() < 0)){
-				j++;
-			}
-			return ( (j+1) == boat.getLength()); 		
-		}
-		
-		else{return false;}
-	}
-	
-	private boolean placeBoat(Boat boat, int sens, int x, int y){
-		boolean res = testPlace(boat, sens, x, y);
-		if (res){ 
-			placeBoatOnCase(boat, sens, x, y);
-		}
-		
-		return res;
 	}
 	
 	public String toString() {
